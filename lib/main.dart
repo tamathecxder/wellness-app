0cb15cc2557f06de9add6b4ec2429383cf962496
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-19 21:06:43 +0700

    chore: set themedata

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:wellness_app/splash_screen.dart';
+import 'package:wellness_app/theme/colors.dart';
+import 'package:wellness_app/theme/style.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+        fontFamily: 'Epilogue',
+        textTheme: const TextTheme(
+          bodyMedium: CustomStyle.defaultTextStyle,
+        ),
+      ),
+      home: const SplashScreen(),
     );
   }
 }
