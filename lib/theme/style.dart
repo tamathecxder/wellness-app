bb842a076acd0a421b5b76c39290333c00e3d576
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-19 21:08:11 +0700

    chore: create style.dart for custom textstyle

---
+++ lib/theme/style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:wellness_app/theme/colors.dart';
+
+class CustomStyle {
+  static const TextStyle defaultTextStyle = TextStyle(
+    color: AppColors.textColor,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle descriptionTextStyle = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Rubik',
+  );
+}
