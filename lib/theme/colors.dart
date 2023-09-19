b6da6874f72ab8a3ea703a6b4e5fdef02a66dbb5
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-19 21:08:30 +0700

    chore: add colors pallete class

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -4,12 +4,14 @@ class AppColors {
   static const Color primaryColor = Color(0xFFFE8235);
   static const Color secondaryColor = Color(0xFFFEF3E7);
   static const Color grayColor = Color(0xFFF4F3F1);
+  static const Color darkGrayColor = Color(0xFF707070);
   static const Color greenColor = Color(0xFF55A06F);
+  static const Color brownColor = Color(0xFF573926);
   static const Color lightPinkColor = Color(0xFFEF5DA8);
   static const Color lightBlueColor = Color(0xFFAEAFF7);
   static const Color lightTealColor = Color(0xFFA0E3E2);
   static const Color lightOrangeColor = Color(0xFFF09E54);
   static const Color lightGreenColor = Color(0xFFC3F2A6);
-  static const Color textColor = Color(0xFF573926);
+  static const Color textColor = Color(0xFF371B34);
   static const Color backgroundColor = Color(0xFFFFFFFF);
 }
