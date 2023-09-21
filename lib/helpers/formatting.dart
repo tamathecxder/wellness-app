f0815e97a61677b7503909bde2b6d425d2024e64
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-21 23:57:45 +0700

    chore: create formatting helper

---
+++ lib/helpers/formatting.dart
@@ -0,0 +1,18 @@
+import 'package:intl/intl.dart';
+
+class Formatting {
+  static String formatTimeAgo(DateTime dateTime) {
+    final now = DateTime.now();
+    final difference = now.difference(dateTime);
+
+    if (difference.inDays > 0) {
+      return DateFormat.yMMMd().format(dateTime);
+    } else if (difference.inHours > 0) {
+      return '${difference.inHours} hours ago';
+    } else if (difference.inMinutes > 0) {
+      return '${difference.inMinutes} minutes ago';
+    } else {
+      return 'Just now';
+    }
+  }
+}
